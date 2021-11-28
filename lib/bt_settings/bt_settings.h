ff959da3e003d81885c43dd3bbaa6dbad5268e65
36kyo <kyotar@gmail.com>
2021-11-28 16:24:57 +0900

    add bt address setting header

---
+++ lib/bt_settings/bt_settings.h
@@ -0,0 +1,6 @@
+#ifndef _BT_SETTING_H_
+#define _BT_SETTING_H_
+
+#define BT_ADDRESS "01:23:45:67:89:AB" // change to your address
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include <Ps3Controller.h>
+// address setting
+// /lib/bt_settings/bt_settings.h
+#include <bt_settings.h>
 
 void setup() {
   // put your setup code here, to run once:
+  Ps3.begin(BT_ADDRESS); // change to your address
+  
 }
 
 void loop() {
