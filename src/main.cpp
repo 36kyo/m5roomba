b42ad96a9d00c7d02492da00587105ec2b3d9261
36kyo <kyotar@gmail.com>
2021-11-28 19:20:53 +0900

    add m5stack lib,code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <Arduino.h>
+#include <M5Stack.h>
+#include <M5GFX.h>
 #include <Ps3Controller.h>
 // address setting
 // /lib/bt_settings/bt_settings.h
@@ -7,9 +8,15 @@
 void onConnect();
 void notify();
 
+M5GFX display;
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  M5.begin();
+  M5.Power.begin(); //Init Power module.
+  display.begin();
+  display.fillScreen(TFT_BLACK);
+  display.printf("m5roomba");
 
   Ps3.attach(notify);
   Ps3.attachOnConnect(onConnect);
@@ -19,12 +26,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(
-    String(Ps3.data.analog.stick.lx) + "\t"
-    + String(Ps3.data.analog.stick.ly) + "\t"
-    + String(Ps3.data.analog.stick.rx) + "\t"
-    + String(Ps3.data.analog.stick.ry)
-  );
+  M5.update();
+  display.fillScreen(TFT_BLACK);
+  display.setColor(TFT_WHITE);
+  display.drawCircle(display.width()/2.0, display.height()/2.0, 100);
+  display.drawLine(0, display.height()/2.0, display.width(), display.height()/2.0);
+  display.drawLine(display.width()/2.0, 0, display.width()/2.0, display.height());
+  display.drawLine(
+    display.width()/2.0, display.height()/2.0,
+    display.width()/2.0 + Ps3.data.analog.stick.lx,
+    display.height()/2.0 + Ps3.data.analog.stick.ly);
+
+  // Serial.println(
+  //   String(Ps3.data.analog.stick.lx) + "\t"
+  //   + String(Ps3.data.analog.stick.ly) + "\t"
+  //   + String(Ps3.data.analog.stick.rx) + "\t"
+  //   + String(Ps3.data.analog.stick.ry)
+  // );
+
+  delay(50);
 }
 
 void onConnect(){
